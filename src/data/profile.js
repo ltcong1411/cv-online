785d390eba2e9ad6d2ebd10ce7240136d66ac324
Cong Le <congle@chotot.vn>
2020-07-04 00:28:03 +0700

    init project

---
+++ src/data/profile.js
@@ -0,0 +1,27 @@
+const profile = {
+  name: 'Cong Le',
+  title: 'Software Developer',
+  mail: 'ltcong1411@gmail.com',
+  phoneNumber: '+8498.111.6265',
+  website: 'thanhcongtechnology.com',
+  linkedin: 'linkedin.com/in/cong-le-1b7681150',
+  github: 'github.com/ltcong1411',
+  imagePath: 'CongLe.jpg',
+  twitter: null,
+  educationDetails: {
+    sectionTitle: '',
+    list: [
+      { name: 'BSc in Electronic and Telecommunication Enginering', school: 'HCMC University Of Technology', date: '2013-2017' },
+    ]
+  },
+  languages: {
+    sectionTitle: 'Languages',
+    list: [{ name: 'Vietnamese', level: 'Native' }, { name: 'English', level: 'Professional' }]
+  },
+  interests: {
+    sectionTitle: '',
+    list: ['Travel', 'Read Book', 'Cooking']
+  }
+};
+
+export default profile;
